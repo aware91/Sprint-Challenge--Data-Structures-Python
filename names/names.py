311b8a1d44823ffe2cc0c750eef56c28873000d7
Anthony Ware <aware91coding@gmail.com>
2020-09-25 21:27:53 -0500

    names mvp

--- names/names.py
+++ names/names.py
@@ -10,13 +10,46 @@ f = open('names_2.txt', 'r')
 names_2 = f.read().split("\n")  # List containing 10000 names
 f.close()
 
-duplicates = []  # Return the list of duplicates in this data structure
+duplicates = []  # Return the list of duplicates in this data structure\
+
+class BSTNode:
+    def __init__(self, value):
+        self.val = value
+        self.r = None
+        self.l = None
+    def insert(self, value):
+        if value < self.val:
+            if self.l is None:
+                self.l = BSTNode(value)
+            else:
+                self.l.insert(value)
+        if value >= self.val:
+            if self.r is None:
+                self.r = BSTNode(value)
+            else:
+                self.r.insert(value)
+    def contains(self, target):
+        if self.val == target:
+            return True
+        elif target < self.val:
+            if self.l and self.l.contains(target):
+                return self.l
+            else:
+                return False
+        else:
+            if self.r and self.r.contains(target):
+                return self.r
+            else:
+                return False
+
 
 # Replace the nested for loops below with your improvements
-# for name_1 in names_1:
-#     for name_2 in names_2:
-#         if name_1 == name_2:
-#             duplicates.append(name_1)
+bst = BSTNode("None")
+for name in names_1:
+    bst.insert(name)
+for name in names_2:
+    if bst.contains(name):
+        duplicates.append(name)
 
 
 end_time = time.time()
