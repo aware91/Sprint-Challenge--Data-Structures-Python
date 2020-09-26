41bac15cc8d7c4d681ecdc5dbc75b37c20dd86e0
Anthony Ware <aware91coding@gmail.com>
2020-09-25 20:46:31 -0500

    ring done

--- ring_buffer/ring_buffer.py
+++ ring_buffer/ring_buffer.py
@@ -1,10 +1,19 @@
 class RingBuffer:
     def __init__(self, capacity):
-        self.data = [None for i in range(capacity)]
+        self.capacity = capacity
+        self.list = []
+        self.current_removal = 0
 
     def append(self, item):
-        self.data.pop(0)
-        self.data.append(item)
+        if len(self.list) < self.capacity:
+            self.list.append(item)
+        elif len(self.list) == self.capacity:
+            self.list.pop(self.current_removal)
+            self.list.insert(self.current_removal, item)
+            if self.current_removal < self.capacity - 1:
+                self.current_removal += 1
+            else:
+                self.current_removal =  0
 
     def get(self):
-        return self.data
+        return self.list
